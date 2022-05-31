04b4118f86b1e91195c93e297fd50252a9c7f46b
skar-J <cutejsm@naver.com>
2022-05-31 13:21:16 +0900

    Initial Commit

---
+++ Shared/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  Shared
+//
+//  Created by 1 on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
